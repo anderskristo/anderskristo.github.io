0d3035317b0ee6eb74fdadfcf0040ba1f4605ea3
Anders Kristoffersson <anderskristo@gmail.com>
2021-12-05 09:14:19 +0100

    more spec

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -8,22 +8,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(200).json({ isPlaying: false });
     }
     
-  const song = await response.json();
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists.map((_artist: any) => _artist.name).join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+    const song = await response.json();
 
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  });
+    if (song.currently_playing_type === 'episode') {
+      return res.status(200).json({ isPlaying: true, type: 'episode' });
+    }
+    
+    const isPlaying = song.is_playing;
+    const title = song.item.name;
+    const artist = song.item.artists.map((_artist: any) => _artist.name).join(', ');
+    const album = song.item.album.name;
+    const albumImageUrl = song.item.album.images[0].url;
+    const songUrl = song.item.external_urls.spotify;
+
+    return res.status(200).json({
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+    });
 
   } catch (error: any) {
     throw new Error(error);
