ce62d1450618f5d165633462ea5c4aa33e6eb80e
Anders Kristoffersson <anders.kristoffersson@adeprimo.se>
2023-12-12 17:00:34 -0500

    Remove next

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as LinkItem } from './LinkItem/LinkItem';
-export { default as Heading } from './Heading/Heading';
-export { default as ArtistListItem } from './ArtistListItem/ArtistListItem';
-export { default as TrackListItem } from './TrackListItem/TrackListItem';
-export { default as NowPlaying } from './NowPlaying/NowPlaying';
--- pages/api/genius/callback.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiResponse, NextApiRequest } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    return res.status(200).json({genius: 'yes'});
-  } catch (error: any) {
-    throw new Error(error);
-  }
-}
